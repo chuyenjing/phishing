fcacbadf7f7ae692eb572f37e46c2f980d1207a3
chuyenjing <95420096+chuyenjing@users.noreply.github.com>
2023-04-25 01:48:08 +0800

    Delete new.js

--- new.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('.btn');
-
-button.addEventListener('click', () => {
-    alert('哈哈');
-});
-function jump() {
-    window.location.href = "https://24h.pchome.com.tw/";
-}
