4afa3597379fdeb207c51a5166dd9ce525c45209
chuyenjing <95420096+chuyenjing@users.noreply.github.com>
2023-04-25 01:48:29 +0800

    Add files via upload

---
+++ new.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', () => {
+    alert('哈哈');
+});
+function jump() {
+    window.location.href = "https://24h.pchome.com.tw/";
+}
